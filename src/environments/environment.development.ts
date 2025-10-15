deca9e6e2baa8a063bba378db06b5003f8513294
Ale13258 <aleja13258@gmail.com>
2025-10-15 16:29:58 -0500

    Update development environment URL to use HTTPS for API endpoint

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  URLS: "http://borboleta.site/api",
+  URLS: "https://borboleta.site/api",
   URL: "assets/data", // Change only the domain part, keeping "/api" intact
   storageURL: "assets", // Change only the laravel primary domain
 };
