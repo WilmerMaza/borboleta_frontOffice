d81eea712dd74217579ca04d0c69e181a1783b63
ren-wmaza <wmaza@rengroup.co>
2025-11-24 23:44:39 -0500

    correcion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  URLS: "https://localhost:3001/api",
+  URLS: "https://borboleta.site/api",
   URL: "assets/data", // Change only the domain part, keeping "/api" intact
   storageURL: "assets", // Change only the laravel primary domain
 };
